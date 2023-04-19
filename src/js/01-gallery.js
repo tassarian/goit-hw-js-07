117c75568f269f822e0154fe4e40dc7816baa024
tassarian <122233433+tassarian@users.noreply.github.com>
2023-04-19 20:32:16 +0300

    js-hw-cm-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const imgCollection = galleryItems
+    .map(({ preview, original, description }) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+  `
+    )
+    .join('')
+   
+gallery.insertAdjacentHTML('afterbegin', imgCollection);
+
+let instance = '';
+
+
+gallery.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (e.target.nodeName === 'IMG') {
+        instance = basicLightbox.create(
+            `<img src="${e.target.dataset.source}" width="800" height="600">`
+        );
+        instance.show();
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if (instance.visible()) {
+        if (e.code === 'Escape') {
+            instance.close();
+        };
+    };
+    
+});
+
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const imgCollection = galleryItems
+    .map(({ original, description, preview }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>
+ `
+    )
+    .join('');
+
+gallery.insertAdjacentHTML('beforeend', imgCollection)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsPosition: 'bottom',
+    captionsDelay: 250,
+    captionsData: 'alt',
+    maxZoom: 0``
+    })
